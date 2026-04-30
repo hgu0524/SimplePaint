538a4a4c4173c26ca7480f9fc1377a3c013d7cde
근우 홍 <kalistar0524@gmail.com>
2026-04-30 10:16:14 +0900

    과제1 코딩과 테스트 완료 Windows Forms 기반 그림판 앱의 기본 UI를 구성하고 도형 선택, 색상 선택, 선 두께 선택 기능을 구현함. PictureBox를 캔버스로 사용하기 위한 준비를 완료하고, ComboBox를 이용한 색상 선택 기능과 TrackBar를 이용한 선 굵기 조절 기능을 구현함. 또한 Line, Rectangle, Circle 버튼을 통해 도형 선택 기능을 구현하고 각 컨트롤의 이벤트를 연결하여 사용자 입력에 따라 상태가 변경되도록 처리함.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
+            picCanvas = new PictureBox();
+            cmbColor = new ComboBox();
+            trbLineWidth = new TrackBar();
+            btnLine = new Button();
+            btnRectangle = new Button();
+            btnCircle = new Button();
+            btnOpenFile = new Button();
+            btnSaveFile = new Button();
+            lblAppName = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)picCanvas).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trbLineWidth).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            picCanvas.Location = new Point(31, 292);
+            picCanvas.Name = "picCanvas";
+            picCanvas.Size = new Size(1221, 504);
+            picCanvas.TabIndex = 0;
+            picCanvas.TabStop = false;
+            // 
+            // cmbColor
+            // 
+            cmbColor.FormattingEnabled = true;
+            cmbColor.Items.AddRange(new object[] { "Black", "", "Red", "", "Blue", "", "Green" });
+            cmbColor.Location = new Point(26, 67);
+            cmbColor.Name = "cmbColor";
+            cmbColor.Size = new Size(183, 40);
+            cmbColor.TabIndex = 1;
+            cmbColor.SelectedIndexChanged += cmbColor_SelectedIndexChanged;
+            // 
+            // trbLineWidth
+            // 
+            trbLineWidth.Location = new Point(6, 54);
+            trbLineWidth.Minimum = 1;
+            trbLineWidth.Name = "trbLineWidth";
+            trbLineWidth.Size = new Size(231, 90);
+            trbLineWidth.TabIndex = 2;
+            trbLineWidth.Value = 2;
+            trbLineWidth.ValueChanged += trbLineWidth_ValueChanged;
+            // 
+            // btnLine
+            // 
+            btnLine.Location = new Point(6, 43);
+            btnLine.Name = "btnLine";
+            btnLine.Size = new Size(110, 101);
+            btnLine.TabIndex = 3;
+            btnLine.Text = "직선";
+            btnLine.UseVisualStyleBackColor = true;
+            btnLine.Click += btnLine_Click;
+            // 
+            // btnRectangle
+            // 
+            btnRectangle.Location = new Point(122, 43);
+            btnRectangle.Name = "btnRectangle";
+            btnRectangle.Size = new Size(110, 101);
+            btnRectangle.TabIndex = 4;
+            btnRectangle.Text = "사각형";
+            btnRectangle.UseVisualStyleBackColor = true;
+            btnRectangle.Click += btnRectangle_Click;
+            // 
+            // btnCircle
+            // 
+            btnCircle.Location = new Point(238, 43);
+            btnCircle.Name = "btnCircle";
+            btnCircle.Size = new Size(110, 101);
+            btnCircle.TabIndex = 5;
+            btnCircle.Text = "원";
+            btnCircle.UseVisualStyleBackColor = true;
+            btnCircle.Click += btnCircle_Click;
+            // 
+            // btnOpenFile
+            // 
+            btnOpenFile.Location = new Point(975, 167);
+            btnOpenFile.Name = "btnOpenFile";
+            btnOpenFile.Size = new Size(126, 114);
+            btnOpenFile.TabIndex = 6;
+            btnOpenFile.Text = "열기";
+            btnOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveFile
+            // 
+            btnSaveFile.Location = new Point(1117, 167);
+            btnSaveFile.Name = "btnSaveFile";
+            btnSaveFile.Size = new Size(126, 114);
+            btnSaveFile.TabIndex = 7;
+            btnSaveFile.Text = "저장";
+            btnSaveFile.UseVisualStyleBackColor = true;
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("맑은 고딕", 25.875F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblAppName.ForeColor = Color.Blue;
+            lblAppName.Location = new Point(47, 25);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(431, 92);
+            lblAppName.TabIndex = 8;
+            lblAppName.Text = "Simple paint";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnLine);
+            groupBox1.Controls.Add(btnRectangle);
+            groupBox1.Controls.Add(btnCircle);
+            groupBox1.Location = new Point(47, 124);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(356, 162);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "도형 선택";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(cmbColor);
+            groupBox2.Location = new Point(439, 124);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(234, 162);
+            groupBox2.TabIndex = 10;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "색 선택";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(trbLineWidth);
+            groupBox3.Location = new Point(701, 124);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(246, 162);
+            groupBox3.TabIndex = 11;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "선 두께";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1429, 833);
+            ClientSize = new Size(1284, 829);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(lblAppName);
+            Controls.Add(btnSaveFile);
+            Controls.Add(btnOpenFile);
+            Controls.Add(picCanvas);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)picCanvas).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trbLineWidth).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox picCanvas;
+        private ComboBox cmbColor;
+        private TrackBar trbLineWidth;
+        private Button btnLine;
+        private Button btnRectangle;
+        private Button btnCircle;
+        private Button btnOpenFile;
+        private Button btnSaveFile;
+        private Label lblAppName;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,51 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace SimplePaint
 {
     public partial class Form1 : Form
     {
+        enum ToolType { Line, Rectangle, Circle }
+
+
+        private ToolType currentTool = ToolType.Line;
+        private Color currentColor = Color.Black;
+        private int currentLineWidth = 2;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void cmbColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbColor.SelectedIndex = 0;
+
+            switch (cmbColor.SelectedIndex)
+            {
+                case 0: currentColor = Color.Black; break;
+                case 1: currentColor = Color.Red; break;
+                case 2: currentColor = Color.Blue; break;
+                case 3: currentColor = Color.Green; break;
+            }
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            currentTool = ToolType.Line;
+        }
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            currentTool = ToolType.Rectangle;
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            currentTool = ToolType.Circle;
+        }
+
+        private void trbLineWidth_ValueChanged(object sender, EventArgs e)
+        {
+            currentLineWidth = trbLineWidth.Value;
         }
     }
 }
